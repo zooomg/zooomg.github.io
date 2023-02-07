da9b4d2debe667a8b6e7c91f26a94ad92ab84a9f
zooomg <msgy95@gmail.com>
2023-02-07 15:37:34 +0900

    addtional line

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const content = '안녕하세요! 주민건 입니다'
-const txt = document.querySelector('.txt');
-let n = 0;
+const content = '안녕하세요!+주민건 입니다 '
+const first_line = document.querySelector('.first_line');
+const second_line = document.querySelector('.second_line');
+let txt = first_line;
+let i = 0;
 
 const typing = function() {
-    txt.innerHTML += content[n++]; //.txt엘리먼트에 위에 content의 변수에 저장한 문자를 순차적으로 뿌리겠다.
-    if(n >= content.length) { 
+    txt.innerHTML += content[i++];
+    if(content[i]==='+'){
+        txt = second_line;
+        i++;
+    }
+    if(i >= content.length) { 
         clearInterval(loop);
     }
 };
